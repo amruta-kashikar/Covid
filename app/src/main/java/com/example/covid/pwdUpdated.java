108fdf303e23413e21ab03319abbea4476d66f88
amruta-kashikar <amrutakashikar2@gmail.com>
2021-04-02 16:41:43 +0530

    added design to xml files

---
+++ app/src/main/java/com/example/covid/pwdUpdated.java
@@ -0,0 +1,14 @@
+package com.example.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pwdUpdated extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwd_updated);
+    }
+}
